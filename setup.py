35c147c95e9219a1697110750abe63fe1e097747
Kelvin Davis <kelvin.davis1@monash.edu>
2024-05-01 13:10:26 +1000

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
 
+with open("README.md", encoding="utf-8", errors="ignore") as fp:
+    long_description = fp.read()
+
 setup(
     name="Eugene",
     version="1.0.0",
     description="A library models to experiment with breeding programs",
     author="Kelvin Davis",
     author_email="Kelvin.Davis1@monash.edu",
-    packages=["eugene"]
+    python_requires=">=3.7",
+    packages=["eugene"],
+    install_requires=[
+        "bitarray"
+        "gurobipy=10.0.1",
+        "minizinc",
+        "eugene_rs",
+    ]
 )
