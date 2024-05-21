b46167d28e94f87d5e7228f8a768c604d6f4f033
Kelvin Davis <kelvin.davis1@monash.edu>
2024-05-21 16:56:38 +1000

    Distribute mincross experiment ready to go

--- experiments/ejor/optimal_times_distribute.py
+++ experiments/ejor/optimal_times_distribute.py
@@ -10,36 +10,28 @@ the full set. Instances where n_loci ≥ 9can sampled by generating and checking
 against a list of previously observed instances.
 """
 
+import os
+import json
+from random import seed
+
 import eugene.utils as eu
 import eugene.solvers.base_min_crossings_astar as east
 import eugene.solvers.base_min_crossings_mip as emip
 import eugene.solvers.base_min_crossings_minizinc as emzn
 
-import time
-import matplotlib.pyplot as plt
+seed(0)
+
+N_LOCI = list(range(2, 11))
+N_INST = 100
+N_TRIALS = 5
+
+INSTANCES = {
+    n_loci: [eu.random_distribute_instance(n_loci) for _ in range(N_INST)]
+    for n_loci in N_LOCI
+}
 
 
-class TimeOut:
-    pass
-
-
-def measure_performance(foo, n_loci):
-    instances = [eu.random_distribute_instance(n_loci)]
-    total_time = 0
-    while total_time < 10:
-        start = time.time()
-        for instance in instances:
-            foo(instance)
-        end = time.time()
-        total_time = end - start
-        result_time = total_time / len(instances)
-        instances.extend(
-            [
-                eu.random_distribute_instance(n_loci)
-                for _ in range(len(instances))
-            ]
-        )
-    return result_time
+INSTANCES = {int(k): v for k, v in json.load(open("./data/optgap_instances.json")).items()}
 
 
 def solver_astar(dist_array):
@@ -80,63 +72,105 @@ SOLVERS = {
     "A*": solver_astar,
 }
 
-RESULTS = {
-    "CP-SAT": [None] * 8,
-    "CP-MIP": [None] * 8,
-    "MIP": [None] * 8,
-    "A*": [None] * 8,
-}
+if __name__ == "__main__":
+    import argparse
+    import time
+    import os
 
-# for n_loci in range(1, 5):
-#     print(n_loci)
-#     print(RESULTS)
-#     RESULTS["A*"][n_loci - 1] = measure_performance(SOLVERS["A*"], n_loci)
-
-# for n_loci in range(1, 5):
-#     print(n_loci)
-#     print(RESULTS)
-#     RESULTS["MIP"][n_loci - 1] = measure_performance(SOLVERS["MIP"], n_loci)
-
-RESULTS = {
-    "CP-SAT": [None, None, None, None, None, None, None, None],
-    "CP-MIP": [None, None, None, None, None, None, None, None],
-    "MIP": [
-        0.0005094455336802639,
-        0.003369288519024849,
-        0.023645638953894377,
-        0.19293437525629997,
-        None,
-        None,
-        None,
-        None,
-    ],
-    "A*": [
-        2.3788530597812496e-05,
-        0.0001384845036227489,
-        0.45167065411806107,
-        109.64520788192749,
-        None,
-        None,
-        None,
-        None,
-    ],
-}
-
-for n_loci in range(1, 9):
-    print(n_loci)
-    print(RESULTS)
-    RESULTS["CP-SAT"][n_loci - 1] = measure_performance(
-        SOLVERS["CP-SAT"], n_loci
+    parser = argparse.ArgumentParser(
+        "Experiment runner for MinCross algorithms on random instances"
     )
-    RESULTS["CP-MIP"][n_loci - 1] = measure_performance(
-        SOLVERS["CP-MIP"], n_loci
-    )
+    parser.add_argument("-s", "--solver")
+    parser.add_argument("-o", "--output_file")
+    parser.add_argument("-l", "--loci")
+    args = parser.parse_args()
+    print(args)
 
+    solver = args.solver
+    output_file = args.output_file
+    loci_string = args.loci
 
-for name, results in RESULTS.items():
-    plt.scatter(list(range(1, 9)), RESULTS[name], label=name)
-plt.title("Runtime vs #genes")
-plt.xlabel("#genes")
-plt.ylabel("Runtime (s)")
-plt.yscale("log")
-plt.show()
+    if output_file is None:
+        output_file = ""
+
+    if os.path.exists(output_file):
+        print(f"output_file exists: {output_file}")
+        exit()
+
+    output_dir = os.path.dirname(output_file)
+    if output_dir != "" and not os.path.exists(output_dir):
+        # make directory
+        os.makedirs(output_dir)
+        # then we can assume that output_file does not exist
+
+    if solver is None:
+        solvers = SOLVERS.items()
+    elif solver.upper() == "ASTAR":
+        solvers = [("A*", SOLVERS["A*"])]
+    elif solver.upper() not in SOLVERS:
+        raise ValueError("solver not available")
+    else:
+        solvers = [(solver.upper(), SOLVERS[solver.upper()])]
+
+    if loci_string is None:
+        loci_string = "2-8"
+
+    if loci_string.isnumeric():
+        N_LOCI = [eval(loci_string)]
+    elif loci_string.count("-") == 1 and all(
+        s.isnumeric() for s in loci_string.split("-")
+    ):
+        s, e = (int(c) for c in loci_string.split("-"))
+        N_LOCI = list(range(s, e + 1))
+    elif loci_string.count("-") == 0 and all(
+        s.isnumeric() for s in loci_string.split(",")
+    ):
+        N_LOCI = [eval(s) for s in loci_string.split(",")]
+    else:
+        raise ValueError("incorrect format for loci set")
+
+    for name, solver in solvers:
+        for n_loci, instances in ((n_loci, INSTANCES[n_loci]) for n_loci in N_LOCI):
+            objectives = [None] * len(instances)
+            times_l1 = [0] * len(instances)
+            times_l2 = [0] * len(instances)
+            start = time.time()
+            for i, inst in enumerate(instances):
+                time_l1 = 0
+                time_l2 = 0
+                for _ in range(N_TRIALS):
+                    objective = solver(inst).objective
+                    time_res = time.time() - time_l1 - start
+                    time_l1 += time_res
+                    time_l2 += time_res ** 2
+                objectives[i] = objective
+                times_l1[i] = time_l1 / N_TRIALS
+                times_l2[i] = time_l2 / N_TRIALS ** 2
+            if output_file == "":
+                print(
+                    json.dumps(
+                        {
+                            "solver": name,
+                            "n_loci": n_loci,
+                            "n_trials": N_TRIALS,
+                            "times_l1": times_l1,
+                            "times_l2": times_l2,
+                            "objectives": objectives,
+                        }
+                    )
+                )
+            else:
+                with open(output_file, "a") as f:
+                    print(
+                        json.dumps(
+                            {
+                                "solver": name,
+                                "n_loci": n_loci,
+                                "n_trials": N_TRIALS,
+                                "times_l1": times_l1,
+                                "times_l2": times_l2,
+                                "objectives": objectives,
+                            }
+                        ),
+                        file=f,
+                    )
