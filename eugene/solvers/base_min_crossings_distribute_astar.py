7859d1a810f7d48b003cac7aa4d180340d1e37ea
Kelvin Davis <273degreeskelvin@gmail.com>
2025-02-19 23:19:45 +1100

    first_full_join and sanitize_dist_array

--- eugene/solvers/base_min_crossings_distribute_astar.py
+++ eugene/solvers/base_min_crossings_distribute_astar.py
@@ -52,7 +52,8 @@ def astar(dist_array, ctx):
         open_list.extend(branching(node, ctx=ctx, simplify_results=True))
 
 
-def branching(state: Node, ctx, simplify_results=True) -> List[Node]:
+def branching(state: Node, ctx) -> List[Node]:
+    
     with ctx.instance.branch() as instance:
         instance["instance"] = state.xs
         instance["nLoci"] = state.n_segments
@@ -78,14 +79,42 @@ def success(state: Node):
     len(state.xs) > 0 and all(x == state.xs[0] for x in state.xs)
 
 
-def _simplify_dist_array(dist_array, n_segments):
-    out = [None] * n_segments
-    n_loci = len(dist_array)
-    i = j = 0
-    while i < n_segments:
-        x = dist_array[j]
-        while j < n_loci and dist_array[j] == x:
-            j += 1
-        out[i] = x
-        i += 1
-    return out
+def _simplify_dist_array(xs, n_segments):
+    xs = xs.copy() # get rid of this for more performance (and instability)
+    mapping = [None] * max(xs) # assuming that xs is 1-indexed
+    mapping[xs[0]] = 0
+    x_max = 0
+    i = j = 1
+    while j < n_segments:
+        if xs[j] != xs[j - 1]:
+            if mapping[xs[j]] is not None:
+                xs[i] = mapping[xs[j]]
+            else:
+                x_max += 1
+                mapping[xs[j]] = x_max
+                xs[i] = x_max
+            i += 1
+        j += 1
+    return xs[:i]
+
+
+def first_full_join_raw(xs):
+    n_loci = len(xs)
+    n_pop = max(xs) + 1 # assumes xs is 0-indexed
+
+    start_points = [n_loci - 1] * n_pop
+    end_points = [0] * n_pop
+    for i, gx in enumerate(xs):
+        end_points[gx] = i
+    for i, gx in reversed(enumerate(xs)):
+        start_points[gx] = i
+
+    for (gx, ex) in enumerate(end_points):
+        if ex < n_loci - 1 and start_points[xs[ex + 1]] == ex + 1:
+            gy = xs[ex + 1]
+            out = xs.copy()
+            for i in range(start_points[gy], end_points[gy] + 1):
+                if xs[i] == gy:
+                    out[i] = gx
+            return (out, gx, gy)
+    return None
